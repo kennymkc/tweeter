4ec88b5844222199bb4aeed61694e2ac30f73fe6
Kennie <Kenneth Chan>
2022-12-06 16:45:52 -0500

    change font for placeholder and console log tweet length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-  // when text is inputted (keyup) 
   $('#tweet-text').on('input', function (e) {
-    console.log($(this).val())
+    console.log(140 - $(this).val().length);
   })
 });
