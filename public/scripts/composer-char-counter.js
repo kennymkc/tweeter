2c6c4b19d8f50215ae5d110b2ad11ecc924bd964
Kennie <Kenneth Chan>
2022-12-06 17:47:34 -0500

    update character count to red if over

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
   $('#tweet-text').on('input', function (e) {
-    console.log(140 - $(this).val().length);
+   
+    const remainingCount = 140 - $(this).val().length;
+    //Traverse to counter using DOM
+    const counter = $(this).siblings('.tweet-button-count').children('.counter');
+   
+    counter.text(remainingCount);
+
+    //counter turns red if below 0
+    if (remainingCount < 0) {
+      counter.addClass('over-characters');
+    } else {
+      counter.removeClass('over-characters');
+    }
+
   })
 });
